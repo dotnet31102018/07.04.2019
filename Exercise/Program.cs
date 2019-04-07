57d94bd7dde7cc8a8deee3e2fc463c778b46bf61
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-04-07 20:36:34 +0300

    Create Program.cs

---
+++ Exercise/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise0704
+{
+    class Program
+    {
+        static List<object> GetEmployeesAdnDepartment()
+        {
+
+            string dbName = @"c:\itay\0407.db";
+
+            List<object> results = new List<object>();
+            using (var connection = new SQLiteConnection($"Data Source = {dbName}; Version=3;"))
+            {
+                connection.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand($"SELECT E.ID, E.NAME AS EMP_NAME, D.NAME AS DEP_NAME FROM EMPLOYEE E JOIN DEPARTMENT D ON E.DEPARTMENT_ID=D.ID", 
+                    connection))
+                {
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var obj = new
+                            {
+                                Employee_Id = (Int64)reader["ID"],
+                                Employee_Name = (string)reader["EMP_NAME"],
+                                Department_Name = (string)reader["DEP_NAME"],
+                            };
+                            results.Add(obj);
+                        }
+                    }
+                }
+            }
+            return results;
+
+        }
+        static void Main(string[] args)
+        {
+            var results = GetEmployeesAdnDepartment();
+           
+            Console.WriteLine();
+        }
+    }
+}
